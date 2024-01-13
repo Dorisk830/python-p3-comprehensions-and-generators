66c8578c46fe47d593b9a93a66b35fc65a27f9bb
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-01-13 22:26:14 +0300

    pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = return_evens(numbers)
+print(result)
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+sentences = ["Hello", "How are you", "Python is great"]
+result = make_exclamation(sentences)
+print(result)
